456fcb2251b1da5a8a3925591a9d91120e6c430b
imanipop <imanipop>
2026-04-01 08:06:31 +0300

    message

--- src/App.js
+++ src/App.js
@@ -11,13 +11,29 @@ import Mpesapayment from './components/Mpesapayment';
 import Footer from './components/Footer';
 
 
+
 function App() {
   return (
     <Router>
     <div className="App">
       <div className='App-header'>
         <h1 className='text-primary'>Welcome to Imani car sales</h1>
-      </div><br/>
+    </div><br/>
+    <nav className='m-2 navbar navbar-expand-md navbar-light bg-light' >
+    <Link to="/"  href='#'className=' ms-2 nav-bar brand nav-link'>Get Cars</Link>
+    <button class="navbar-toggler" data-bs-target="#navbarcollapse" data-bs-toggle="collapse">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarcollapse">
+      <div class="navbar-nav ms-auto">
+    <Link to="/signup" className='text-dark ms-2 nav-link'>Signup</Link> 
+    <Link to="/signin" className='text-dark ms-2 nav-link'>Signin</Link>
+    <Link to="/addcars" className='text-dark ms-2 nav-link'>AddCar</Link>
+    <Link to="/getcars" className='text-dark ms-2 nav-link'>GetCar</Link>
+    </div>
+    </div>
+    </nav>
+
       {/* Linking routes */}
       <nav>
         <Link to="/signup" className='btn btn-outline-primary ms-2'>Sign up</Link>
@@ -33,8 +49,9 @@ function App() {
         <Route path='/addcars' element={<Addcars/>}/>
         <Route path='/makepayment' element={<Mpesapayment/>}/>
       </Routes> <br />
-
+      
       <Footer/>
+      
     </div>
     </Router>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+  <nav className='m-2 navbar navbar-expand-md navbar-light bg-light' >
+    <Link to="/"  href='#'className=' ms-2 nav-bar brand nav-link'>Get Novel</Link>
+    <button class="navbar-toggler" data-bs-target="#navbarcollapse" data-bs-toggle="collapse">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarcollapse">
+      <div class="navbar-nav ms-auto">
+    <Link to="/signup" className='text-dark ms-2 nav-link'>Signup</Link> 
+    <Link to="/signin" className='text-dark ms-2 nav-link'>Signin</Link>
+    <Link to="/addcars" className='text-dark ms-2 nav-link'>AddCar</Link>
+    <Link to="/getcars" className='text-dark ms-2 nav-link'>GetCar</Link>
+    </div>
+    </div>
+    </nav>
